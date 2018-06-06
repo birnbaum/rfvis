bb28848cb7402f961c8817ef23ad634e8a95f9fb
Philipp Wiesner <mail@philippwiesner.com>
2018-06-07 01:32:39 +0200

    formatting, linting and minor bugfixes

--- render.js
+++ render.js
@@ -1,66 +1,66 @@
 const D3Node = require('d3-node');
 
 function bar({
-  data,
-  selector: _selector = '#chart',
-  container: _container = `<div id="chart"></div>`,
-  style: _style = '',
-  width: _width = 960,
-  height: _height = 500,
-  margin: _margin = { top: 20, right: 20, bottom: 30, left: 40 },
-  barColor: _barColor = 'steelblue',
-  barHoverColor: _barHoverColor = 'brown',
-}) {
-  const _svgStyles = `
-    .bar { fill: ${_barColor}; }
-    .bar:hover { fill: ${_barHoverColor}; }
-  `;
+                 data,
+                 selector: _selector = '#chart',
+                 container: _container = `<div id="chart"></div>`,
+                 style: _style = '',
+                 width: _width = 960,
+                 height: _height = 500,
+                 margin: _margin = { top: 20, right: 20, bottom: 30, left: 40 },
+                 barColor: _barColor = 'steelblue',
+                 barHoverColor: _barHoverColor = 'brown',
+             }) {
+    const _svgStyles = `
+        .bar { fill: ${_barColor}; }
+        .bar:hover { fill: ${_barHoverColor}; }
+    `;
 
-  const d3n = new D3Node({
-    selector: _selector,
-    styles: _svgStyles + _style,
-    container: _container
-  });
+    const d3n = new D3Node({
+        selector: _selector,
+        styles: _svgStyles + _style,
+        container: _container
+    });
 
-  const d3 = d3n.d3;
+    const d3 = d3n.d3;
 
-  const width = _width - _margin.left - _margin.right;
-  const height = _height - _margin.top - _margin.bottom;
+    const width = _width - _margin.left - _margin.right;
+    const height = _height - _margin.top - _margin.bottom;
 
-  // set the ranges
-  const x = d3.scaleBand()
-          .range([0, width])
-          .padding(0.1);
+    // set the ranges
+    const x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
 
-  const y = d3.scaleLinear()
-          .range([height, 0]);
+    const y = d3.scaleLinear()
+        .range([height, 0]);
 
-  const svg = d3n.createSVG(_width, _height)
-    .append('g')
-    .attr('transform', `translate(${_margin.left}, ${_margin.top})`);
+    const svg = d3n.createSVG(_width, _height)
+        .append('g')
+        .attr('transform', `translate(${_margin.left}, ${_margin.top})`);
 
-  x.domain(data.map((d) => d.key));
-  y.domain([0, d3.max(data, (d) => d.value)]);
+    x.domain(data.map((d) => d.key));
+    y.domain([0, d3.max(data, (d) => d.value)]);
 
-  // append the rectangles for the bar chart
-  svg.selectAll('.bar')
-      .data(data)
-      .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('x', (d) => x(d.key))
-      .attr('width', x.bandwidth())
-      .attr('y', (d) => y(d.value))
-      .attr('height', (d) => height - y(d.value));
+    // append the rectangles for the bar chart
+    svg.selectAll('.bar')
+        .data(data)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', (d) => x(d.key))
+        .attr('width', x.bandwidth())
+        .attr('y', (d) => y(d.value))
+        .attr('height', (d) => height - y(d.value));
 
-  // add the x Axis
-  svg.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(x));
+    // add the x Axis
+    svg.append('g')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(x));
 
-  // add the y Axis
-  svg.append('g').call(d3.axisLeft(y));
+    // add the y Axis
+    svg.append('g').call(d3.axisLeft(y));
 
-  return d3n;
+    return d3n;
 }
 
 const fs = require('fs');
@@ -73,22 +73,22 @@ const data = d3.csvParse(csvString);
 output('./output', bar({ data: data }));
 
 function output(outputName, d3n) {
-  const svg2png = require('svg2png');
-  
-  if (d3n.options.canvas) {
-    const canvas = d3n.options.canvas;
-    console.log('canvas output...', canvas);
-    canvas.pngStream().pipe(fs.createWriteStream(outputName+'.png'));
-    return;
-  }
+    const svg2png = require('svg2png');
 
-  fs.writeFile(outputName+'.html', d3n.html(), function () {
-    console.log('>> Done. Open "'+outputName+'.html" in a web browser');
-  });
+    if (d3n.options.canvas) {
+        const canvas = d3n.options.canvas;
+        console.log('canvas output...', canvas);
+        canvas.pngStream().pipe(fs.createWriteStream(outputName+'.png'));
+        return;
+    }
 
-  var svgBuffer = new Buffer(d3n.svgString(), 'utf-8');
-  svg2png(svgBuffer)
-    .then(buffer => fs.writeFile(outputName+'.png', buffer))
-    .catch(e => console.error('ERR:', e))
-    .then(err => console.log('>> Exported: '+outputName+'.png"'));
-};
+    fs.writeFile(outputName+'.html', d3n.html(), function () {
+        console.log('>> Done. Open "'+outputName+'.html" in a web browser');
+    });
+
+    const svgBuffer = new Buffer(d3n.svgString(), 'utf-8');
+    svg2png(svgBuffer)
+        .then(buffer => fs.writeFile(outputName+'.png', buffer))
+        .catch(e => console.error('ERR:', e))
+        .then(err => console.log('>> Exported: '+outputName+'.png"'));
+}
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
-const path = require("path");
-const express = require('express')
-const app = express()
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
 
 if(process.argv.length !== 3) {
-    throw "Usage: node server.js <data_folder>";
+    throw 'Usage: node server.js <data_folder>';
 }
 
-const statisticsDir = process.argv[2] + "/statistics";
-const summaryFile = process.argv[2] + "/summary.txt";
+const statisticsDir = process.argv[2] + '/statistics';
+const summaryFile = process.argv[2] + '/summary.txt';
 
-app.get('/',     (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
+app.get('/',     (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 app.get('/data', (req, res) => res.json(createForest(summaryFile, statisticsDir)));
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 
 /**
@@ -29,32 +29,32 @@ app.listen(3000, () => console.log('Example app listening on port 3000!'))
  *          ]
  *      }]
  *  }
- * 
+ *
  * @param {string} summaryFile - Path to the overall summary file
  * @param {string} statisticsDirectory - Path to the directory containing the tree statistics files
  * @returns {Object}
  */
 function createForest(summaryFile, statisticsDirectory) {
-    const summary = fs.readFileSync(summaryFile, "utf-8")
-    const summaryParts = summary.split("\n\n");
+    const summary = fs.readFileSync(summaryFile, 'utf-8');
+    const summaryParts = summary.split('\n\n');
 
-    const totalStrength = Number.parseFloat(summaryParts[0])
-    const correlationMatrix = parseCorrelationMatrix(summaryParts[2])
+    const totalStrength = Number.parseFloat(summaryParts[0]);
+    const correlationMatrix = parseCorrelationMatrix(summaryParts[2]);
 
     const treeFiles = fs.readdirSync(statisticsDirectory);
-    const treeStrengths = summaryParts[1].split("\n").map(Number.parseFloat)
-    
+    const treeStrengths = summaryParts[1].split('\n').map(Number.parseFloat);
+
     // Sanity check
     if (treeFiles.length !== treeStrengths.length) {
-        console.log(treeFiles, treeStrengths)
-        console.log(treeFiles.length, treeStrengths.length)
+        console.log(treeFiles, treeStrengths);
+        console.log(treeFiles.length, treeStrengths.length);
         throw `Number of trees noted in ${summaryFile} (${treeFiles.length}) is inconsistend ` +
-              `to the amount of files in ${statisticsDirectory} (${treeStrengths.length})`;
+        `to the amount of files in ${statisticsDirectory} (${treeStrengths.length})`;
     }
 
     const trees = treeFiles.map((treeFile, index) => {
         const treeFilePath = path.resolve(statisticsDirectory, treeFile);
-        const content = fs.readFileSync(treeFilePath, "utf-8");
+        const content = fs.readFileSync(treeFilePath, 'utf-8');
         return {
             strength: treeStrengths[index],
             nodes: parseStatisticsContent(content)
@@ -71,23 +71,23 @@ function createForest(summaryFile, statisticsDirectory) {
 /**
  * Parses a given correlation matrix text to a two-dimensional array of floats
  * @param {string} text - correlation matrix text
- * @returns {float[][]}
+ * @returns {number[][]}
  */
 function parseCorrelationMatrix(text) {
     text = text.replace(/\[|\]/g, '');  // Remove [] brackets from string
-    return text.split(";\n").map(line => 
-        line.split(",").map(Number.parseFloat)
+    return text.split(';\n').map(line =>
+        line.split(',').map(Number.parseFloat)
     );
 }
 
 /**
- * Parses a given tree statistics text file content into an internal touple representation
+ * Parses a given tree statistics text file content into an internal tuple representation
  * @param {string} text - tree statistics text
- * @returns {int[]} 
+ * @returns {number[][]}
  */
 function parseStatisticsContent(text) {
-    return text.trim().split("\n").map(line => {
-        const fields = line.split(";");
+    return text.trim().split('\n').map(line => {
+        const fields = line.split(';');
         return [
             Number.parseInt(fields[0]),  // height
             Number.parseInt(fields[1]),  // samples
