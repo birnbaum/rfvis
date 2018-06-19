ff89f9ec94c798b6255ab8462c76a96cf65118ac
Philipp Wiesner <mail@philippwiesner.com>
2018-06-19 12:50:16 +0200

    copying index.html via rollup plugin instead of cp

--- rollup.backend.js
+++ rollup.backend.js
@@ -1,12 +1,12 @@
 import fs from "fs";
-const {dependencies, devDependencies} = JSON.parse(fs.readFileSync('./package.json'));
+const {dependencies, devDependencies} = JSON.parse(fs.readFileSync("./package.json"));
 const external = ["fs", "path", "util", ...Object.keys(dependencies), ...Object.keys(devDependencies)];
 
 export default {
-    input: './index.js',
+    input: "./index.js",
     output: {
-        file: './dist/index.js',
-        format: 'cjs',
+        file: "./dist/index.js",
+        format: "cjs",
         sourcemap: true
     },
     external: external
--- rollup.frontend.js
+++ rollup.frontend.js
@@ -1,19 +1,23 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import sass from 'rollup-plugin-scss';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import copy from "rollup-plugin-copy";
+import sass from "rollup-plugin-scss";
 
 export default {
-    input: './src/main.js',
+    input: "./src/main.js",
     output: {
-        file: './dist/public/js/main.js',
-        format: 'iife',
+        file: "./dist/public/js/main.js",
+        format: "iife",
         sourcemap: true
     },
     plugins: [
         resolve(),
         commonjs(),
+        copy({
+            "index.html": "dist/index.html"
+        }),
         sass({
-            output: './dist/public/css/style.css'
+            output: "./dist/public/css/style.css"
         })
     ]
 };
