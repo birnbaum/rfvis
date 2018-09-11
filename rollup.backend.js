c9deeb705551b78d4b8edac3c44791000d4d56b2
Philipp Wiesner <mail@philippwiesner.com>
2018-09-11 12:29:12 +0200

    updated dependencies

--- rollup.backend.js
+++ rollup.backend.js
@@ -1,5 +1,4 @@
 import fs from "fs";
-import copy from "rollup-plugin-copy";
 const {dependencies, devDependencies} = JSON.parse(fs.readFileSync("./package.json"));
 const external = ["fs", "path", "util", "child_process", ...Object.keys(dependencies), ...Object.keys(devDependencies)];
 
@@ -10,10 +9,5 @@ export default {
         format: "cjs",
         sourcemap: true
     },
-    external: external,
-    plugins: [
-        copy({
-            "./src/_compute_coordinates.js": "dist/_compute_coordinates.js"  // Copy module called in subprocess
-        })
-    ]
+    external: external
 };
